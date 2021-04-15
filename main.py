19e549c89db5ea9f4e2cb10fa042047f7fdfbcf0
Wojtek <wojtas5127>
2021-04-15 13:00:32 +0200

    zadania skończone

---
+++ main.py
@@ -0,0 +1,104 @@
+import numpy as np
+
+#zad1
+
+# a = np.arange(3)
+# b = np.array([4,5,6])
+# print(a)
+# print(b)
+#
+# c = a * b
+# print(c)
+
+
+#zad2
+
+# a = np.arange(9).reshape(3,3)
+# b = np.arange(16).reshape(4,4)
+# print(a)
+# print(b)
+# print(a.min(axis=0))
+# print(a.min(axis=1))
+# print(b.min(axis=0))
+# print(b.min(axis=1))
+
+#zad3
+
+# a = np.arange(3)
+# b = np.array([4,5,6])
+# print(a)
+# print(b)
+# print(np.dot(a,b))
+
+#zad4
+
+# a = np.array([2,3,4])
+# b = np.array([2.,-1.,4.20])
+# print(a)
+# print(b)
+# c = a * b
+# print(c)
+
+#zad5
+
+# s = np.arange(6).reshape(2,3)
+# a = np.sin(s)
+# print(s)
+# print(a)
+
+#zad6
+
+# d = np.arange(6).reshape(2,3)
+# b = np.cos(d)
+# print(d)
+# print(b)
+# print("")
+
+#zad7
+
+# c = a + b
+# print(c)
+
+#zad8
+
+# a = np.arange(9).reshape(3,3)
+# print(a)
+#
+# for b in a:
+#     print(b)
+#     print("")
+
+#zad9
+
+# a = np.arange(9).reshape(3,3)
+# print(a)
+#
+# for b in a.flat:
+#     print(b)
+#     print("")
+
+#zad10
+
+# a = np.arange(81).reshape(9,9)
+# print(a)
+
+#zad11
+
+# a = np.arange(12).reshape(3,4)
+# b = np.arange(12).reshape(4,3)
+# c = np.arange(12).reshape(2,6)
+# d = a.ravel()
+# e = b.ravel()
+# f = c.ravel()
+#
+# print(a)
+# print("")
+# print(b)
+# print("")
+# print(c)
+# print("")
+# print(d)
+# print("")
+# print(e)
+# print("")
+# print(f)
